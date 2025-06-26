f547a51fe910a003f74860957f4656377a0b35f3
joel2995 <joelsundarsingh2005@gmail.com>
2025-06-26 15:19:14 +0530

    Updations

--- taxiBooking/TaxiBooking.java
+++ taxiBooking/TaxiBooking.java
@@ -5,6 +5,60 @@ import java.util.ArrayList;
 public class TaxiBooking
 {
     private static ArrayList<Taxi> taxiList = new ArrayList<Taxi>();
-    private static int taxListLimit=4 , idGenerator=1;
-    
+    private static int taxiListLimit=4 , idGenerator=1;
+    private static ArrayList<Taxi> taxiBookedHistory = new ArrayList<Taxi>();
+
+    public static String booking(char pickupLocation , char dropLocation , int pickupTime) throws CloneNotSupportedException
+    {
+        if(taxiList.size()<taxiListLimit)
+        {
+            taxiList.add(new Taxi());
+        }
+        int min=Integer.MAX_VALUE;
+        Taxi taxiReady=null;
+        for(Taxi t: taxiList)
+        {
+            if(t.getDropTime()<=pickupTime && Math.abs(pickupLocation - t.getCurrentLocation())<=min)
+            {
+                if(Math.abs(pickupLocation - t.getCurrentLocation())==min)
+                {
+                    if(taxiReady!=null && t.getEarnings()<taxiReady.getEarnings())
+                    {
+                        taxiReady=t;
+                    }
+                }
+            
+                else{
+                    taxiReady=t;
+                    min=Math.abs(pickupLocation - t.getCurrentLocation());
+                }
+            }
+        }
+        if(taxiReady!=null)
+        {
+            taxiReady.setCustomerId(idGenerator++);
+			taxiReady.setPickupTime(pickupTime);
+			taxiReady.setPickupLocation(pickupLocation);
+			taxiReady.setDropLocation(dropLocation);
+			taxiReady.setCurrentLocation(dropLocation);
+			taxiReady.setDropTime(pickupTime + Math.abs(dropLocation-pickupLocation));
+			taxiReady.setEarnings((taxiReady.getEarnings()) + (Math.abs(dropLocation-pickupLocation)*15-5)*10 + 100);
+			taxiReady.setTaxiId(taxiList.indexOf(taxiReady)+1);
+			taxiBookedHistory.add((Taxi)taxiReady.clone());
+        }
+
+        return taxiReady!=null?"Taxi number "+taxiReady.getTaxiId()+" is booked!":"Taxis not available";
+
+
+    }
+    public static void display()
+    {
+        System.out.println("------------------");
+        for(Taxi t : taxiBookedHistory){
+
+            System.out.println(t.toString());
+            System.out.println("------------------");
+        }
+    }
+
 }
