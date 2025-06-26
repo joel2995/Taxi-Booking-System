0da6b24f9fee903f1a711ede63993bbe1db19679
joel2995 <joelsundarsingh2005@gmail.com>
2025-06-26 15:03:10 +0530

    Initial Commit

---
+++ taxiBooking/TaxiBooking.java
@@ -0,0 +1,10 @@
+package taxiBooking;
+
+import java.util.ArrayList;
+
+public class TaxiBooking
+{
+    private static ArrayList<Taxi> taxiList = new ArrayList<Taxi>();
+    private static int taxListLimit=4 , idGenerator=1;
+    
+}
